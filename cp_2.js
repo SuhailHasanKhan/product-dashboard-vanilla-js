c8d509aac590cb308ab1901320729800518b9537
Suhail Hasan Khan <shasankhan@usf.edu>
2025-10-17 17:36:25 -0400

    created a function to Fetch products with then()

--- cp_2.js
+++ cp_2.js
@@ -2,3 +2,16 @@
 
 const apiLink = "https://www.course-api.com/javascript-store-products";
 
+function fetchProductsThen() {
+    
+    fetch(apiLink)
+      .then((res) => res.json())
+      .then((data) => {
+        data.forEach((product) => {
+            console.log("Product Name: ", product.fields.name);
+        });
+      })
+      .catch((err) => {
+        console.error("The following error has occured:", err.message);
+      });
+}
