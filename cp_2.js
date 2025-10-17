2c3f5ab4b106b3562ef60fc5c7f89a47119f471b
Suhail Hasan Khan <shasankhan@usf.edu>
2025-10-17 18:05:07 -0400

    created Function to fetch products using Async and also created a fucntion to display products.

--- cp_2.js
+++ cp_2.js
@@ -15,3 +15,41 @@ function fetchProductsThen() {
         console.error("The following error has occured:", err.message);
       });
 }
+
+async function fetchProductsAsync() {
+    try {
+        let results = await fetch(apiLink);
+        let details = await res.json();
+        displayProducts(data);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+function displayProducts(products) {
+    let prodContainer = document.getElementById("product-container");
+    let fiveProducts = products.slice(0, 5);
+    fiveProducts.forEach((p) => {
+        let { name, price, image } = p.fields;
+        let imgLink = image && image[0] && image[0].url ? image[0].url : "";
+        let listing = document.createElement("div");
+        listing.className = "product-listing";
+        let img = document.createElement("img");
+        img.src = imgLink;
+        img.alt = name;
+
+        let prodTitle = document.createElement("div");
+        prodTitle.className = "product-title";
+        prodTitle.textContent = name;
+
+        let prodPrice = document.createElement("div");
+        prodPrice.className = "product-price";
+        prodPrice.textContent = `$${price}`;
+
+        listing.appendChild(img);
+        listing.appendChild(prodTitle);
+        listing.appendChild(prodPrice);
+        prodContainer.appendChild(listing);
+
+    });
+}
